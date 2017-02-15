e02d5c2bfd41ea052a0808e1f4764b15557ccfc6
Nick Nash <nicholas.nash@gmail.com>
2017-02-15 22:04:01 +0000

    Add untested hashed read indicator, spike implementation

---
+++ HashedReadIndicator.cs
@@ -0,0 +1,47 @@
+using System.Threading;
+
+namespace SharpLeftRight
+{
+    class HashedReadIndicator : IReadIndicator
+    {
+        private int[] _occupancyCounts;
+        private int _paddingPower;
+
+        public HashedReadIndicator(int numEntries, int paddingPower)
+        {
+            _occupancyCounts = new int[numEntries << paddingPower];
+            _paddingPower = paddingPower;
+        }
+
+        private int GetIndex()
+        {
+            var threadId = Thread.CurrentThread.ManagedThreadId; 
+            var result = threadId.GetHashCode() << _paddingPower;
+            return result;
+        }
+        
+        public void Arrive()
+        {
+            int index = GetIndex();
+            Interlocked.Increment(ref _occupancyCounts[index]);
+        }
+
+        public void Depart()
+        {
+            int index = GetIndex();
+            Interlocked.Decrement(ref _occupancyCounts[index]);
+        }
+
+        public bool IsOccupied()
+        {
+            for(int i = 0; i < _occupancyCounts.Length; ++i)
+            {
+                if(_occupancyCounts[i << _paddingPower] > 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
