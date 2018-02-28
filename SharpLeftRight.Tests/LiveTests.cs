80b41eb717f7c41a2cb3813495e3f040b79efae6
Nick Nash <nicholas.nash@gmail.com>
2018-02-28 07:36:09 +0000

    Add a threaded live test

--- SharpLeftRight.Tests/LiveTests.cs
+++ SharpLeftRight.Tests/LiveTests.cs
@@ -4,6 +4,7 @@ using Xunit;
 using RelaSharp;
 using SharpLeftRight;
 using System.Linq;
+using System.Threading;
 
 namespace SharpLeftRight.Tests
 {
@@ -22,5 +23,42 @@ namespace SharpLeftRight.Tests
 
             Xunit.Assert.True(equal);
         }
+
+        [Fact]
+        public void Read_DuringAndAfterWriting_FinalReadIsCorrect()
+        {
+            var barrier = new Barrier(4);
+            var leftRightList = LeftRightBuilder.Build(new List<int>(), new List<int>());            
+            int total = 0;
+            bool writeFinished = false;
+            WaitCallback reader = _ => 
+                { 
+                    barrier.SignalAndWait();
+                    while(!writeFinished)
+                    {
+                        total = leftRightList.Read<int>(l => { int t = 0; l.ForEach(v => { t += v; }); return t; });
+                    }
+                    total = leftRightList.Read<int>(l => { int t = 0; l.ForEach(v => { t += v; }); return t; });
+                    barrier.SignalAndWait();
+                };
+            int maxWritten = 1000;
+            WaitCallback writer = _ => 
+                {
+                    barrier.SignalAndWait();
+                    for(int i = 0; i <= maxWritten; ++i)
+                    {
+                        leftRightList.Write(l => l.Add(i));
+                    }
+                    writeFinished = true;
+                    barrier.SignalAndWait();
+                };
+            ThreadPool.QueueUserWorkItem(reader);
+            ThreadPool.QueueUserWorkItem(reader);
+            ThreadPool.QueueUserWorkItem(writer);
+            barrier.SignalAndWait();
+            barrier.SignalAndWait();
+            Xunit.Assert.Equal(maxWritten * (maxWritten + 1) / 2, total);
+        }
+        
     }
 }
